f2a9f9b382c2a161f8032fc540bfb64f6bcb2d0a
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-08 12:43:26 -0700

    Print using Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - prints the sentence "Programming is like building a multilingual puzzle"
  * Return: 0 if successful, else nonzero
- * */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
