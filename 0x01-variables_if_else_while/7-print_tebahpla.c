543476bc8b7e110edf676ccb02165931ff6d69e8
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-09 15:07:01 -0700

    print reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in reverse
+ * Return: o if succesful
+ */
+
+int main(void)
+{
+	char n = 'z';
+
+	while (n != 'a')
+	{
+		putchar(n);
+	}
+	n--;
+	putchar('\n');
+	return (0);
+}
