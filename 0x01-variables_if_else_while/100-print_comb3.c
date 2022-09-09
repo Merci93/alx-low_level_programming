bf02c655a26d82733f01514ffac9b239b27e1406
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-09 15:44:20 -0700

    print combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print combination 3
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		fir (j = 49; j <= 57; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
