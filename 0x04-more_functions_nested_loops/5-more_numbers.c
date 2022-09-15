711ca2b667f2d9d6fb80184aeb6e3a4895898271
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-15 12:35:38 -0700

    print 0 to 14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the number 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int x,y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		-putchar('\n');
+	}
+}
