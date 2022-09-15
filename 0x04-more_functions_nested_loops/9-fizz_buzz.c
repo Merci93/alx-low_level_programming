8ce5556f9ce753c5000f6c76b3f34b611fec7629
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-15 13:48:48 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,18 @@
 
 int main(void)
 {
-	int x;
+	int i;
 
-	for (x = 1; x <= 100; x++)
+	for (i = 1; i <= 100 ; i++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if (x % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (x % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
-			printf("%d ");
+			printf("%d ", i);
 	}
 	printf("\n");
 
