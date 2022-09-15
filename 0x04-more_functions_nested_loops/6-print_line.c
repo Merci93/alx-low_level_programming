bed3cdee96d0ae9a4b7c3d5b3a572021cc024e7f
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-15 12:46:42 -0700

    Draw a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,22 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: length of the line
+ * @n: number of times the character _ should be printed
  */
-
 void print_line(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 		int i;
-		
+
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
+
 }
