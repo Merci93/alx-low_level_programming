f6a2aa4dd6907d9980b548b2c19caab9ccc4d3e8
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-09-19 16:00:51 -0700

    copy string from a pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,35 +4,24 @@
  * _strcpy - copies a sring pointed to by src to the buffer dest
  * @src: pointer to string to be copied
  * @dest: pointer to string destination
- * Return: pointer to dest
+ * Return: dest
  */
-
-/*char *_strcpy(char *dest, char *src)
-{
-	char *tmp = dest;
-
-	while (*src)
-	{
-		*dest++ = *src++;
-	}
-	return (tmp);
-}*/
 
 char *_strcpy(char *dest, char *src)
 {
-        int len = 0;
-        int i;
+	int len = 0;
+	int i;
 
-        while (src[len] != '\0')
-        {
-                len++;
-        }
+	while (src[len] != '\0')
+	{
+		len++;
+	}
 
-        for (i = 0; i < len; i++)
-        {
-                dest[i] = src[i];
-        }
-        dest[i] = '\0';
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
 
-        return (dest);
+	return (dest);
 }
