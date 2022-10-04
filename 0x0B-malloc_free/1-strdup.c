1d7f32c114b14b56b3a176580ec861ed98625d58
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2022-10-04 08:34:16 -0700

    a function that returns the memory address of a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - a function that returns a pointer to a newly allocated space in memory
+ * @*str: string whose pointer dublicate is to be returned
+ * Return: Null if str is NULL or pointer to dublicate.
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+
+	return (duplicate);
+}
